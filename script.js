0629691537ae9c9452e97864b5b956d7687bda20
peakyblinders1998 <111987419+peakyblinders1998@users.noreply.github.com>
2023-10-10 15:59:16 +0000

    Add files via upload
    todo list

---
+++ script.js
@@ -0,0 +1,26 @@
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+const generateTemplate = (todo) => {
+  const html = `<li class="list-group-item d-flex justify-content-between">
+  <span>${todo}</span>
+  <i class="bi bi-trash-alt delete text-medium">x</i>
+</li>`;
+
+  list.innerHTML += html;
+};
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+// delete todos
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    if (e.target.classList.contains("delete")) {
+      e.target.parentElement.remove();
+    }
+  }
+});
